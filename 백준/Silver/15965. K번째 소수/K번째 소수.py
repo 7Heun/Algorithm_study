3bb3a15c436cc175c5227b11a4c0709c756238de
GAEUN <97210854+7Heun@users.noreply.github.com>
2024-05-21 09:13:18 +0900

    [Silver II] Title: K번째 소수, Time: 2800 ms, Memory: 94752 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15965. K번째 소수/K번째 소수.py
+++ 백준/Silver/15965. K번째 소수/K번째 소수.py
@@ -1,14 +1,16 @@
 import sys
+import math
 input = sys.stdin.readline
 k = int(input())
-prime = [2]
-num = 3
-while len(prime) < k:
-    for i in prime:
-        if i**2 > num:
-            prime.append(num)
+max_range = int(k * math.log(k) * 1.2)
+prime = [True] * max_range
+prime[0] = prime[1] = False
+count = 0
+for i in range(2, max_range):
+    if prime[i]:
+        count += 1
+        if count == k:
+            print(i)
             break
-        if num % i == 0:
-            break
-    num += 2
-print(prime[-1])
+        for j in range(i*2, max_range, i):
+            prime[j] = False
