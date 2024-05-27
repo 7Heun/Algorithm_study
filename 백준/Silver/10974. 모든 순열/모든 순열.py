c0617682affdbcb5dd9733cd7ce8e873895a0cdf
GAEUN <97210854+7Heun@users.noreply.github.com>
2024-05-27 09:03:39 +0900

    [Silver III] Title: 모든 순열, Time: 140 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.py
+++ 백준/Silver/10974. 모든 순열/모든 순열.py
@@ -1,19 +1,7 @@
+import itertools
 import sys       
 input = sys.stdin.readline
 N = int(input())
 arr = [i for i in range(1, N+1)]
-visited = [False] * N
-result = []
-
-def dfs(depth):
-    if depth == N:
-        print(' '.join(map(str, result)))
-        return
-    for i in range(N):
-        if not visited[i]:
-            visited[i] = True
-            result.append(arr[i])
-            dfs(depth+1)
-            result.pop()
-            visited[i] = False
-dfs(0)
+for perm in itertools.permutations(arr):
+    print(*perm)
