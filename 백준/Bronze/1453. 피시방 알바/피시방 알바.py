e72006418fc7ee4124bf18b5ee6e1014227329d8
GAEUN <97210854+7Heun@users.noreply.github.com>
2024-06-11 16:56:57 +0900

    [Bronze II] Title: 피시방 알바, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1453. 피시방 알바/피시방 알바.py
@@ -0,0 +1,12 @@
+N = int(input())
+arr = list(map(int, input().split()))
+computer = [0] * 101
+cnt = 0
+
+for i in arr:
+    if computer[i] == 0:
+        computer[i] = 1
+    else:
+        cnt += 1
+
+print(cnt)
