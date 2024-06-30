b33a831800a448438866b9d2d29f349404405165
GAEUN <97210854+7Heun@users.noreply.github.com>
2024-06-30 20:26:42 +0900

    [Silver II] Title: DFS와 BFS, Time: 308 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(graph, start):
+    visited = []
+    stack = [start]
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            visited.append(node)
+            stack.extend(sorted(graph[node], reverse=True))
+    return visited
+
+def bfs(graph, start):
+    visited = []
+    queue = [start]
+    while queue:
+        node = queue.pop(0)
+        if node not in visited:
+            visited.append(node)
+            queue.extend(sorted(graph[node]))
+    return visited
+
+N, M, V = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+print(*dfs(graph, V))
+print(*bfs(graph, V))
