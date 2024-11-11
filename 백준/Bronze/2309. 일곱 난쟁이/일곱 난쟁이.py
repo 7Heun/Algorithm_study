46cf69958f083e27bea4bbb053fee39cf5109bbd
GAEUN <97210854+7Heun@users.noreply.github.com>
2024-11-11 20:14:40 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -2,14 +2,11 @@ import sys
 input = sys.stdin.readline
 dwarfs = [int(input()) for _ in range(9)]
 dwarfs.sort()
-total = sum(dwarfs)
-def find_dwarfs(dwarfs, total):
-    for d in dwarfs:
-        for w in dwarfs:
-            if d != w and total - d - w == 100:
-                dwarfs.remove(d)
-                dwarfs.remove(w)
-                return dwarfs
-dwarfs = find_dwarfs(dwarfs, total)
+for d in dwarfs:
+    for w in dwarfs:
+        if sum(dwarfs) - d - w == 100:
+            dwarfs.remove(d)
+            dwarfs.remove(w)
+            break
 for dwarf in dwarfs:
     print(dwarf)
