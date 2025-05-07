d5a7bb01ff45bc39b8a8f603fc208e243a8cb645
GAEUN <97210854+7Heun@users.noreply.github.com>
2025-05-07 15:48:16 +0900

    [Silver III] Title: 개미, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/10158. 개미/개미.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+w, h = map(int, input().split())
+p, q = map(int, input().split())
+t = int(input())
+
+x = (p + t) % (w * 2)
+y = (q + t) % (h * 2)
+if x > w: x = 2 * w - x
+if y > h: y = 2 * h - y
+
+print(x, y)
