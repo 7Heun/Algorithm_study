44496068fde103262b225eb3cc70db2c5779e2ba
GAEUN <97210854+7Heun@users.noreply.github.com>
2025-05-09 19:33:14 +0900

    [Silver IV] Title: 수열, Time: 92 ms, Memory: 33976 KB -BaekjoonHub

---
+++ 백준/Silver/2491. 수열/수열.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = list(map(int, input().split()))
+inc = dec = ans = 1
+
+for i in range(1, N):
+    if arr[i] >= arr[i-1]: inc += 1
+    else: inc = 1
+    if arr[i] <= arr[i-1]: dec += 1
+    else: dec = 1
+    ans = max(ans, inc, dec)
+
+print(ans)
