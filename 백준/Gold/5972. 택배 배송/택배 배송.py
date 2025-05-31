423f91d3314d7e76a310f0721dce4a66cfec398b
GAEUN <97210854+7Heun@users.noreply.github.com>
2025-05-31 15:53:48 +0900

    [Gold V] Title: 택배 배송, Time: 536 ms, Memory: 120484 KB -BaekjoonHub

---
+++ 백준/Gold/5972. 택배 배송/택배 배송.py
@@ -0,0 +1,26 @@
+import sys, heapq
+input = sys.stdin.readline
+
+def dijkstra(start):
+    dist = [float('inf')] * (N + 1)
+    hq = [(start, 0)]
+    while hq:
+        node, cost = heapq.heappop(hq)
+        if cost > dist[node]:
+            continue
+        for next_node, weight in graph[node]:
+            new_cost = cost + weight
+            if new_cost < dist[next_node]:
+                dist[next_node] = new_cost
+                heapq.heappush(hq, (next_node, new_cost))
+    return dist
+
+N, M = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+    graph[b].append((a, c))
+
+dist = dijkstra(1)
+print(dist[N])
