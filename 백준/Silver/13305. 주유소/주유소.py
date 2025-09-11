9c963b63935f87d2399295b7bd7012aa18efa2a6
GAEUN <97210854+7Heun@users.noreply.github.com>
2025-09-11 17:32:50 +0900

    [Silver III] Title: 주유소, Time: 136 ms, Memory: 135244 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -5,27 +5,12 @@ n = int(input())
 roads = list(map(int, input().split()))
 stations = list(map(int, input().split()))
 
-'''
-주유소에 도착한 시점에 남은 이동거리가 있다면, 해당 주유소 가격과 다음 주유소 가격 비교
-다음 주유소보다 비싸면 최소 주유, 싸면 다음 싼 주유소가 나올 때까지 이동 가능하게 주유
-'''
-stations.pop()
+# 현재 주유소에서 지금까지 본 것 중 가장 싼 가격으로 다음 도시까지 주유
+min_price = stations[0]
 total = 0
-i = 0
-while True:
-    if i >= len(stations)-1: break
-    if stations[i] > stations[i+1]:
-        total += stations[i] * roads[i]
-        i += 1
-        continue
-    next_station = i
-    for j in range(i+1, len(stations)):
-        if stations[i] > stations[j]:
-            next_station = j
-            break
-        if j == len(stations)-1:
-            next_station = len(stations)
-    for k in range(i, next_station):
-        total += stations[i] * roads[k]
-    i = next_station
+
+for i in range(n-1):
+    if stations[i] < min_price:
+        min_price = stations[i]
+    total += min_price * roads[i]
 print(total)
