08b614cb5268591c9f4ad5393195a18f2561a414
GAEUN <97210854+7Heun@users.noreply.github.com>
2025-10-07 20:24:39 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 104 ms, Memory: 123120 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -1,15 +1,24 @@
-def print_star(n):
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+def draw_star(n):
     if n == 1:
         return ['*']
-    stars = print_star(n//3)
+    stars = draw_star(n // 3)
     arr = []
+
+    # 위쪽 3등분 (***)
     for s in stars:
-        arr.append(s*3)
+        arr.append(s * 3)
+    # 중간쪽 3등분 (* *)
     for s in stars:
-        arr.append(s + ' '*(n//3) + s)
+        arr.append(s + ' ' * (n // 3) + s)
+    # 아래쪽 3등분 (***)
     for s in stars:
-        arr.append(s*3)
+        arr.append(s * 3)
+    
     return arr
 
-n = int(input())
-print('\n'.join(print_star(n)))
+print('\n'.join(draw_star(N)))
