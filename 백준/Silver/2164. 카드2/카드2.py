7b7858f11cd865f43af26da9b6bdd64d8d0fc8f6
GAEUN <97210854+7Heun@users.noreply.github.com>
2025-10-10 16:05:37 +0900

    [Silver IV] Title: 카드2, Time: 128 ms, Memory: 117200 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,10 +1,11 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
+from collections import deque
 
-N = int(input())
-cards = deque([i for i in range(1, N+1)])
-while len(cards) > 1:
-    cards.popleft()
-    cards.append(cards.popleft())
-print(cards[0])
+n = int(input())
+arr = deque(i for i in range(1, n+1))
+
+while len(arr) > 1:
+    arr.popleft()
+    arr.rotate(-1)
+print(arr[0])
