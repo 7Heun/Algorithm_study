fca202e18ccab38c6f7d6e15fda221b73b266d50
GAEUN <97210854+7Heun@users.noreply.github.com>
2025-10-14 16:33:15 +0900

    [Silver II] Title: DFS와 BFS, Time: 144 ms, Memory: 241052 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,32 +1,35 @@
 import sys
 input = sys.stdin.readline
+from collections import defaultdict, deque
+sys.setrecursionlimit(10**5+5)
 
-def dfs(graph, start):
+n, m, v = map(int, input().split())
+graph = defaultdict(list)
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def dfs(g, start):
     visited = []
     stack = [start]
     while stack:
         node = stack.pop()
         if node not in visited:
             visited.append(node)
-            stack.extend(sorted(graph[node], reverse=True))
+            stack.extend(sorted(g[node], reverse=True))
     return visited
 
-def bfs(graph, start):
+def bfs(g, start):
     visited = []
-    queue = [start]
+    queue = deque([start])
     while queue:
-        node = queue.pop(0)
+        node = queue.popleft()
         if node not in visited:
             visited.append(node)
-            queue.extend(sorted(graph[node]))
+            queue.extend(sorted(g[node]))
     return visited
 
-N, M, V = map(int, input().split())
-graph = [[] for _ in range(N + 1)]
-for _ in range(M):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-
-print(*dfs(graph, V))
-print(*bfs(graph, V))
+print(*dfs(graph, v))
+print(*bfs(graph, v))
