3788bc4d45c58dbfe6cd73877952a53e2122b4b4
GAEUN <97210854+7Heun@users.noreply.github.com>
2025-10-22 22:43:25 +0900

    [Gold V] Title: 토마토, Time: 420 ms, Memory: 220876 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,32 +1,37 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
+from collections import deque
 
-M, N = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(N)]
-
-dq = deque()
-for i in range(N):
-    for j in range(M):
-        if arr[i][j] == 1:
-            dq.append((i, j, 0))
+m, n = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
 
 dx = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
 max_day = 0
 
-while dq:
-    cy, cx, day = dq.popleft()
-    for i in range(4):
-        ny, nx = cy + dy[i], cx + dx[i]
-        if 0 <= ny < N and 0 <= nx < M and arr[ny][nx] == 0:
-            arr[ny][nx] = 1
-            dq.append((ny, nx, day+1))
-            max_day = max(max_day, day+1)
+def bfs():
+    q = deque()
+    max_day = 0
 
-for a in arr:
-    if 0 in a:
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] == 1:
+                q.append((i, j, 0))
+    while q:
+        r, c, day = q.popleft()
+        for i in range(4):
+            nr, nc = r + dy[i], c + dx[i]
+            if 0 <= nr < n and 0 <= nc < m and board[nr][nc] == 0:
+                board[nr][nc] = 1
+                q.append((nr, nc, day+1))
+                max_day = max(max_day, day+1)
+    return max_day
+
+ans = bfs()
+
+for row in board:
+    if 0 in row:
         print(-1)
         break
 else:
-    print(max_day)
+    print(ans)
