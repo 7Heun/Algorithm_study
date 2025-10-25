965fa220c68bc7645bc79cbf1784fb6be971f622
GAEUN <97210854+7Heun@users.noreply.github.com>
2025-10-25 20:08:16 +0900

    [Gold IV] Title: 탈출, Time: 124 ms, Memory: 114436 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.py
+++ 백준/Gold/3055. 탈출/탈출.py
@@ -1,41 +1,46 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
+from collections import deque
 
-R, C = map(int, input().split())
-board = [list(input()) for _ in range(R)]
+r, c = map(int, input().split())
+board = [list(input().rstrip()) for _ in range(r)]
+
 water = deque()
-hedgehog = deque()
-for i in range(R):
-    for j in range(C):
-        if board[i][j] == 'S':
-            hedgehog.append((i, j, 0))
-        elif board[i][j] == '*':
+q = deque()
+water_time = [[-1] * c for _ in range(r)]
+visited = [[-1] * c for _ in range(r)]
+
+for i in range(r):
+    for j in range(c):
+        if board[i][j] == '*':
             water.append((i, j))
+            water_time[i][j] = 0
+        elif board[i][j] == 'S':
+            q.append((i, j))
+            visited[i][j] = 0
 
-dx = [0, 0, 1, -1]
+dx = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
-visited = [[False] * C for _ in range(R)]
 
-while hedgehog:
-    for _ in range(len(water)):
-        r, c = water.popleft()
-        for i in range(4):
-            nr, nc = r + dy[i], c + dx[i]
-            if 0 <= nr < R and 0 <= nc < C and board[nr][nc] == '.':
-                board[nr][nc] = '*'
-                water.append((nr, nc))
+while water:
+    y, x = water.popleft()
+    for d in range(4):
+        nx, ny = x + dx[d], y + dy[d]
+        if 0 <= nx < c and 0 <= ny < r:
+            if board[ny][nx] not in ('X', 'D') and water_time[ny][nx] == -1:
+                water_time[ny][nx] = water_time[y][x] + 1
+                water.append((ny, nx))
 
-    for _ in range(len(hedgehog)):
-        hr, hc, cnt = hedgehog.popleft()
-        for i in range(4):
-            nhr, nhc = hr + dy[i], hc + dx[i]
-            if 0 <= nhr < R and 0 <= nhc < C:
-                if board[nhr][nhc] == 'D':
-                    print(cnt+1)
-                    exit()
-                if board[nhr][nhc] == '.' and not visited[nhr][nhc]:
-                    visited[nhr][nhc] = True
-                    hedgehog.append((nhr, nhc, cnt+1))
-        
-print('KAKTUS')
+while q:
+    y, x = q.popleft()
+    for d in range(4):
+        nx, ny = x + dx[d], y + dy[d]
+        if 0 <= nx < c and 0 <= ny < r:
+            if board[ny][nx] == 'D':
+                print(visited[y][x]+1)
+                sys.exit()
+            if board[ny][nx] == '.' and visited[ny][nx] == -1:
+                if water_time[ny][nx] == -1 or water_time[ny][nx] > visited[y][x] + 1:
+                    visited[ny][nx] = visited[y][x] + 1
+                    q.append((ny, nx))
+print("KAKTUS")
