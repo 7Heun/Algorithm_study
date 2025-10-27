344872308e43ac344f3f4b7145f8ce07f9d60d49
GAEUN <97210854+7Heun@users.noreply.github.com>
2025-10-27 22:07:41 +0900

    [Silver II] Title: 최소 힙, Time: 160 ms, Memory: 114204 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.py
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -1,14 +1,12 @@
-import heapq, sys
+import sys
 input = sys.stdin.readline
+import heapq
 
-N = int(input())
+n = int(input())
 hq = []
-for _ in range(N):
+for _ in range(n):
     x = int(input())
-    if x == 0:
-        if hq:
-            print(heapq.heappop(hq))
-        else:
-            print(0)
-    else:
+    if x:
         heapq.heappush(hq, x)
+    else:
+        print(heapq.heappop(hq) if hq else 0)
