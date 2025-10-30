0235a8c5b7d56ddadd51a9847965a762118f3d20
GAEUN <97210854+7Heun@users.noreply.github.com>
2025-10-30 16:19:55 +0900

    [Gold III] Title: 파티, Time: 152 ms, Memory: 111936 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,37 +1,31 @@
 import sys, heapq
 input = sys.stdin.readline
+from math import inf
+
+n, m, x = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+reversed_graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    s, e, t = map(int, input().split())
+    graph[s].append((e, t))
+    reversed_graph[e].append((s, t))
 
 def dijkstra(start, graph):
-    dist = [float('inf')] * (N + 1)
+    dist = [inf] * (n+1)
     dist[start] = 0
-    hq = [(0, start)]   # 다음 방문 노드
+    hq = [(0, start)]
 
     while hq:
-        cost, node = heapq.heappop(hq)
-        if cost > dist[node]:   # 이미 더 짧은 경로 있으면 무시
-            continue
-        for next_node, weight in graph[node]:
-            new_cost = cost + weight
-            if new_cost < dist[next_node]:
-                dist[next_node] = new_cost
-                heapq.heappush(hq, (new_cost, next_node))
+        now_t, now = heapq.heappop(hq)
+        if now_t > dist[now]: continue
+        for nxt, nxt_t in graph[now]:
+            if now_t + nxt_t < dist[nxt]:
+                dist[nxt] = now_t + nxt_t
+                heapq.heappush(hq, (dist[nxt], nxt))
     return dist
 
-N, M, X = map(int, input().split())
-go_graph = [[] for _ in range(N+1)]
-back_graph = [[] for _ in range(N+1)]
+go = dijkstra(x, reversed_graph)
+back = dijkstra(x, graph)
 
-for _ in range(M):
-    s, e, t = map(int, input().split())
-    back_graph[s].append((e, t))
-    go_graph[e].append((s, t))
-
-max_dist = 0
-back_dist = dijkstra(X, back_graph) # X -> i
-go_dist = dijkstra(X, go_graph) # i -> X (역방향)
-
-for i in range(1, N+1):
-    if go_dist[i] != float('inf') and back_dist != float('inf'):
-        max_dist = max(max_dist, go_dist[i] + back_dist[i])
-
-print(max_dist)
+ans = max(go[i] + back[i] for i in range(1, n+1))
+print(ans)
