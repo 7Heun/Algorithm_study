f95eb9cb1d5c9413b3421ddaa453b391fb8dc6d7
GAEUN <97210854+7Heun@users.noreply.github.com>
2025-11-06 17:41:32 +0900

    [Gold IV] Title: 네트워크 연결, Time: 332 ms, Memory: 251288 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**5+5)
+
+n = int(input())
+m = int(input())
+edges = []
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    edges.append((c, a, b))
+
+edges.sort()
+parent = [i for i in range(n+1)]
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+def union(a, b):
+    aroot = find(a)
+    broot = find(b)
+    if aroot == broot:
+        return False
+    parent[broot] = aroot
+    return True
+
+ans = 0
+for c, a, b in edges:
+    if union(a, b):
+        ans += c
+print(ans)
