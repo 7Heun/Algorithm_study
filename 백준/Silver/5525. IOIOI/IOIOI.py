ad1912989a8fb66657cf82ef1e8cd374a62c7cdd
GAEUN <97210854+7Heun@users.noreply.github.com>
2026-03-19 13:14:43 +0900

    [Silver I] Title: IOIOI, Time: 116 ms, Memory: 113408 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+s = input().strip()
+
+ans = 0
+cnt = 0
+i = 0
+
+while i < m-2:
+    if s[i] == 'I' and s[i+1] == 'O' and s[i+2] == 'I':
+        cnt += 1
+        if cnt >= n:
+            ans += 1
+        i += 2
+    else:
+        cnt = 0
+        i += 1
+
+print(ans)
