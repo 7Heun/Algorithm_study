1c384da04bb0f5b6b9d98fa24394b8a8a5c4aa9e
GAEUN <97210854+7Heun@users.noreply.github.com>
2026-04-10 18:09:04 +0900

    [Gold I] Title: 2048 (Easy), Time: 184 ms, Memory: 113136 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
@@ -1,33 +1,42 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-board = [list(map(int, input().split())) for _ in range(N)]
+'''
+상하좌우 이동, 같은 숫자 나오면 합치기
+-> board rotate 시켜서 한 방향으로만 이동
+dfs로, depth 체크 (최대 5번 이동)
+'''
+n = int(input())
+board = [list(map(int, input().split())) for _ in range(n)]
 
 def move_left(board):
     new_board = []
     for row in board:
-        new_row = [r for r in row if r != 0]    #  빈칸 제거
+        # 빈칸 제거
+        new_row = [r for r in row if r != 0]
+        # 숫자 비교
         result = []
-        skip = False
-        for i in range(len(new_row)):
-            if skip:
-                skip = False
-                continue
-            if i+1 < len(new_row) and new_row[i] == new_row[i+1]:
-                result.append(new_row[i] * 2)
-                skip = True
+        idx = 0
+        while idx < len(new_row)-1:
+            if new_row[idx] == new_row[idx+1]:
+                result.append(new_row[idx]*2)
+                idx += 2
             else:
-                result.append(new_row[i])
+                result.append(new_row[idx])
+                idx += 1
+        # 마지막 숫자 처리
+        if idx == len(new_row)-1:
+            result.append(new_row[idx])
         result += [0] * (len(row) - len(result))
         new_board.append(result)
     return new_board
 
 def rotate(board):
-    new_board = [[0] * N for _ in range(N)]
-    for i in range(N):
-        for j in range(N):
-            new_board[j][N-i-1] = board[i][j]
+    new_board = [[0] * n for _ in range(n)]
+    # 90도 회전
+    for i in range(n):
+        for j in range(n):
+            new_board[n-j-1][i] = board[i][j]
     return new_board
 
 def dfs(board, depth):
@@ -35,13 +44,14 @@ def dfs(board, depth):
         return max(map(max, board))
     result = 0
     for i in range(4):
+        # board 복사
         rotated_board = [row[:] for row in board]
+        # 회전
         for _ in range(i):
             rotated_board = rotate(rotated_board)
+        # 숫자 합치기
         next_board = move_left(rotated_board)
-        
         result = max(result, dfs(next_board, depth+1))
     return result
 
-ans = dfs(board, 0)
-print(ans)
+print(dfs(board, 0))
