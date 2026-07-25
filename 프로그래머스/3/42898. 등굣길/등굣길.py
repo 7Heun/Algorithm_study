2228b587878075d1a550f555705fec9d1f1c3d87
GAEUN <97210854+7Heun@users.noreply.github.com>
2026-07-25 18:40:30 +0900

    [level 3] Title: 등굣길, Time: 3.00 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,15 +1,23 @@
+'''
+우,하로만 움직여야 함
+최단경로의 개수
+물이면 못 감
+1-based
+dp[n][m]
+'''
+from collections import deque
+
 def solution(m, n, puddles):
-    dp = [[0] * m for _ in range(n)]    # 전체 좌표의 가능한 경로 수를 0으로 초기화
-    for i in range(n):
-        for j in range(m):
-            if [j+1, i+1] in puddles:   # 현재 좌표가 물에 잠긴 경우 건너뜀
+    dp = [[0] * (m+1) for _ in range(n+1)]
+    dp[1][1] = 1
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            # 시작점이면 패스
+            if i == 1 and j == 1: continue
+            # 물웅덩이면 패스
+            if [j, i] in puddles: 
+                dp[i][j] = 0
                 continue
-            if i == 0 and j == 0:    # 시작점은 가능한 경로 수 1
-                dp[i][j] = 1
-            elif i == 0:    # 현재 좌표가 0번째 행이면 왼쪽에서만 올 수 있음
-                dp[i][j] = dp[i][j-1]
-            elif j == 0:
-                dp[i][j] = dp[i-1][j]   # 현재 좌표가 0번째 열이면 위에서만 올 수 있음
-            else:
-                dp[i][j] = dp[i-1][j] + dp[i][j-1]  # 현재 좌표의 위와 왼쪽의 경로 수 더함
-    return dp[-1][-1] % 1000000007
+            dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000007
+    
+    return dp[n][m]
