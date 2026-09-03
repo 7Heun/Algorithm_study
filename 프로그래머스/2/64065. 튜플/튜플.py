a4fc3291d71cacbfba26dbe385979b6d3ef2b0ad
GAEUN <97210854+7Heun@users.noreply.github.com>
2026-09-03 16:18:58 +0900

    [level 2] Title: 튜플, Time: 128.01 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -7,18 +7,14 @@
 def solution(s):
     # 주어진 집합 문자열을 리스트로 만드는 작업
     new_s = []
-    s = s[1:-2].split('},')
-    for a in s:
-        if a[0] == '{':
-            new_s.append(list(map(int, a[1:].split(','))))
-        else:
-            new_s.append(list(map(int, a.split(','))))
-    sorted_s = sorted(new_s, key=lambda x: len(x))
-    ans = sorted_s[0]
+    tmp = s.lstrip('{').rstrip('}').split('},{')
+    lst = [list(map(int, t.split(','))) for t in tmp]
+    sorted_lst = sorted(lst, key=len)
+    ans = sorted_lst[0]
     
     # 다음 집합에서 현재 집합에 없는 숫자 찾아서 append
-    for i in range(1, len(sorted_s)):
-        for n in sorted_s[i]:
-            if n not in sorted_s[i-1]:
+    for i in range(1, len(sorted_lst)):
+        for n in sorted_lst[i]:
+            if n not in sorted_lst[i-1]:
                 ans.append(int(n))
     return ans
